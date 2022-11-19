2aa0cbef09c76f6aab56ace013f3689444526f90
SandraMarq <118024145+SandraMarq@users.noreply.github.com>
2022-11-19 16:47:50 +0000

    Deploying to main from  @ 9b8942959903d88efcfc7cadefc06eb33626a4d9 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -3101,7 +3101,7 @@ var mapView = function () {
     $('#map').sdgMap({
       indicatorId: indicatorId,
       mapOptions: {"disaggregation_controls":false,"minZoom":5,"maxZoom":10,"tileURL":"","tileOptions":{"id":"","accessToken":"","attribution":""},"colorRange":"chroma.brewer.BuGn","noValueColor":"#f0f0f0","styleNormal":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#888888","dashArray":""},"styleHighlighted":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#111111","dashArray":""},"styleStatic":{"weight":2,"opacity":1,"fillOpacity":0,"color":"#172d44","dashArray":"5,5"}},
-      mapLayers: [{"subfolder":"map/entidades-federativas","label":"indicator.map","min_zoom":0,"max_zoom":20,"staticBorders":false}],
+      mapLayers: [{"subfolder":"map/entidades-federativas","label":"indicator.map","min_zoom":0,"max_zoom":20,"staticBorders":false},{"subfolder":"map/municipios","label":"indicator.map","min_zoom":0,"max_zoom":20,"staticBorders":false}],
       precision: precision,
       precisionItems: precisionItems,
       decimalSeparator: decimalSeparator,
